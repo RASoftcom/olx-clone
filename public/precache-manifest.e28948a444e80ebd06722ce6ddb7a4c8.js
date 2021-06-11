7e2426f74b178ae1335e2e57ec13e632f7a10091
RASoftcom <80659052+RASoftcom@users.noreply.github.com>
2021-06-11 11:06:36 +0500

    done

---
+++ public/precache-manifest.e28948a444e80ebd06722ce6ddb7a4c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e21e0e94daf74c6f14e6bb5eaceaeb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6078833bb6d804c468",
+    "url": "/static/css/2.3937aacf.chunk.css"
+  },
+  {
+    "revision": "12bea9058d0e4dc5b6db",
+    "url": "/static/css/main.5c664a5f.chunk.css"
+  },
+  {
+    "revision": "7e6078833bb6d804c468",
+    "url": "/static/js/2.7a337297.chunk.js"
+  },
+  {
+    "revision": "6c04f1ee26a2420b9912a10c85342fa9",
+    "url": "/static/js/2.7a337297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bea9058d0e4dc5b6db",
+    "url": "/static/js/main.01136e20.chunk.js"
+  },
+  {
+    "revision": "d6e907c365da0acbace7",
+    "url": "/static/js/runtime-main.8bbf9e43.js"
+  },
+  {
+    "revision": "c761cf277ae5f3e976de025067f95f13",
+    "url": "/static/media/olx-logo-1.c761cf27.png"
+  }
+]);
